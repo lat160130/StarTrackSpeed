805292ec3c89c13838fa6edc1daebe745652423a
lat160130 <lat160130@tamu.edu>
2022-02-04 16:17:36 -0600

    created QUEST.m this is cpu only, will remain post push

---
+++ QUEST.c
@@ -0,0 +1,8 @@
+// Title:   Quest.c
+// Author:  Leon Tannenbaum
+// Project: StarTrack Speed
+// Date of initial write: Feb 4th 2022
+//
+
+
+// Goal: This is CPU only non parallized version of the QUEST attitude determination algorithm.
