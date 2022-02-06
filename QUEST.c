9d93f8f3932aad7a7611770516c11f5b61325762
lat160130 <lat160130@tamu.edu>
2022-02-06 11:03:19 -0600

    added more comments and the base structure for QUEST_CPU.c

--- QUEST.c
+++
@@ -1,8 +0,0 @@
-// Title:   Quest.c
-// Author:  Leon Tannenbaum
-// Project: StarTrack Speed
-// Date of initial write: Feb 4th 2022
-//
-
-
-// Goal: This is CPU only non parallized version of the QUEST attitude determination algorithm.
---
+++ QUEST_CPU.c
@@ -0,0 +1,27 @@
+// Title:   QUEST_CPU.c
+// Author:  Leon Tannenbaum
+// Project: StarTrack Speed
+// Date of initial write: Feb 4th 2022
+//
+
+
+// Goal:  Proceed through the QUEST algorithm in C optimizing as much as possible without using the GPU.
+// Input: AB- Acceleration   vector in body frame [3x1]
+//        MB- Magnetic field vector in body frame [3x1]
+//        GB- Gravity vector  in navigation frame [3x1]
+//        MN- Magnetic vector in navigation frame [3x1]
+
+
+// == Import Header File Block ==================================================================== ⋁ Import Header File Block
+#include <stdio.h>
+// ================================================================================================ ⋀ Import Header File Block
+
+
+// == Main Function =============================================================================== ⋁ Main Function
+int main(){
+
+
+} // int main()
+// ================================================================================================ ⋀ Main Function
+
+
